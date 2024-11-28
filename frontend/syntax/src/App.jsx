8d5afb4c41546ee565e1c0c0ad36b482769ef197
fhumulani mulaudzi <115710782+fhumulani12@users.noreply.github.com>
2024-11-28 11:45:02 +0200

    feat(authentication): authentiocation

--- frontend/syntax/src/App.jsx
+++ frontend/syntax/src/App.jsx
@@ -1,14 +1,25 @@
 
 import './App.css'
 import Register from './components/register'
+import Login from './components/Login'
 import './Global.scss'
+import { Route, Routes } from 'react-router-dom'
+import Authentication from './components/Authentication'
+
 
 function App() {
 
 
   return (
     <div>
-   <Register/>
+      <Routes>
+        <Route path='/'  element={<Register/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Authentication" element={<Authentication/>}/>
+
+
+
+      </Routes>
     </div>
   )
 }
